71125bd114fe74aad0aeeb244e29e2b5be06bf09
ebrehault <ebrehault@gmail.com>
2013-11-18 19:17:31 +0100

    allow search method in formulas

--- rapido/core/__init__.py
+++ rapido/core/__init__.py
@@ -1,1 +1,6 @@
+from zope.security.checker import NamesChecker, defineChecker
+from rapido.core.database import Database
+
+defineChecker(Database, NamesChecker(['search']))
+
 ANNOTATION_KEY = "RAPIDO_ANNOTATION"
