f5aa13cb7b1de97250ef5de877484ea7e0023183
ebrehault <ebrehault@gmail.com>
2014-06-16 16:51:27 +0200

    fix settings importation

--- rapido/core/database.py
+++ rapido/core/database.py
@@ -44,6 +44,7 @@ class Database(Index):
     @acl_check('create_document')
     def create_document(self, docid=None):
         record = self.storage.create()
+        import pdb; pdb.set_trace( )
         doc = IDocument(record)
         if not docid:
             docid = str(hash(record))
