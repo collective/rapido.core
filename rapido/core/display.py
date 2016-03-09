91aada5c0103fd26edc2ed20641fe51b42f07507
Eric BREHAULT <ebrehault@gmail.com>
2016-03-09 21:01:26 +0100

    Fix #13 bad copy/paste

--- rapido/core/display.py
+++ rapido/core/display.py
@@ -112,16 +112,6 @@ class Display(object):
             else:
                 if not self.app.acl.has_permission('view'):
                     raise Unauthorized()
-                # execute submitted actions
-                block = record.block
-                if block:
-                    actions = [key for key in request.keys()
-                        if key.startswith("action.")]
-                    for action_id in actions:
-                        element_id = action_id[7:]
-                        if block.elements.get(element_id, None):
-                            redirect = block.compute_element(
-                                element_id, {'record': record})
                 result = record.display(edit=editmode)
         else:
             raise NotAllowed()
